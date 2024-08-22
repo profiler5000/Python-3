2f8806edbc35411af518303de0d3d6de9f4780cf
profiler5000 <profiler5000@gmail.com>
2024-08-22 06:17:32 +0000

    Initial commit

---
+++ Ress/loader.py
@@ -0,0 +1,9 @@
+import yaml
+
+with open("Ress/stats.yaml", "r") as file:
+	data = yaml.safe_load(file)
+
+
+with open("Ress/menues.yaml", "r") as file:
+	data2 = yaml.safe_load(file)
+	
---
+++ main.py
@@ -0,0 +1,6 @@
+from Ress.loader import data
+
+print(data2["items"])
+#["items"]["player"]["mame"])
+
+
